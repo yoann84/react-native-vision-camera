de40be0f2ce4f99f546f9ec25f63358171c211bf
Yoann <yoann.le-hir@orange.fr>
2025-10-20 17:24:47 +0200

    Fix app.plugin.js to use built files

--- package/app.plugin.js
+++ package/app.plugin.js
@@ -1,3 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-module.exports = require('./expo-plugin/withVisionCamera')
-//module.exports = require('./lib/commonjs/expo-plugin/withVisionCamera')
+module.exports = require('./lib/commonjs/expo-plugin/withVisionCamera')
